936b11ca459d6adb5b5b4ab5913cf25c9b18734e
Xiaoyongsheng <929113374@qq.com>
2018-07-08 13:22:44 +0800

    new change1

---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,70 @@
+<template>
+	<div class="recommend">
+		<div class="recommend-content">
+			<div class="slider-wrapper">
+				<slider>
+					<div v-for="item in recommends">
+						<a :href="item.linkUrl">
+							<img :src="item.picUrl">
+						</a>
+					</div>
+				</slider>
+			</div>
+			<div class="recommend-list">
+				<h1 class="list-title">热门歌单推荐</h1>
+				<ul></ul>
+			</div>
+		</div>
+	</div>
+</template>
+<script type="text/ecmascript-6">
+	import Slider from 'base/slider/slider'
+	import {getRecommend} from 'api/recommend'
+	import {ERR_OK} from 'api/config'
+
+	export default {
+		data() {
+			return {
+				recommends: []
+			}
+		},
+		created() {
+			this._getRecommend()
+		},
+		methods: {
+			_getRecommend() {
+				getRecommend().then((res) => {
+					if(res.code === ERR_OK) {
+						this.recommends = res.data.slider
+					}
+				})
+			}
+		},
+		components:{
+			Slider
+		}
+	}	
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+	@import "~common/stylus/variable"
+
+	.recommend
+		position: fixed
+		width: 100%
+		top: 88px
+		bottom: 0
+		.recommend-content
+			height: 100%
+			overflow: hidden
+			.slider-wrapper
+				position: relative
+				width: 100%
+				overflow: hidden
+			.recommend-list
+				.list-title
+					height: 65px
+					line-height: 65px
+					text-align: center
+					font-size: $font-size-medium
+					color: $color-theme
+</style>
